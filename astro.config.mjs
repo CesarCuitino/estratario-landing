67efa354cfc5b30fcba0e7499be61c75edeca2bb
César Cuitiño <c.cuitino84@gmail.com>
2026-07-10 00:59:10 -0400

    perf: inline all CSS, move Google Fonts to <link> with preconnect

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
 	site: "https://estratario.com",
 	output: "static",
+	build: {
+		inlineStylesheets: "always",
+	},
 	i18n: {
 		defaultLocale: "es",
 		locales: ["es", "en"],
